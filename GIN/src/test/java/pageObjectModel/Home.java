0702d01e8072c5e172aec149266cd09016b7e1f2
ginzabe <admin@ginzabe.com>
2023-11-18 07:58:43 -0500

    update documentation

--- GIN/src/test/java/pageObjectModel/Home.java
+++ GIN/src/test/java/pageObjectModel/Home.java
@@ -4,21 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class Home {  
-	
-    //declare the variable
-	
-WebDriver driver;
-	
-    //Constructor
+public class Home {
 
-  public Home (WebDriver driver) {
-	  this.driver = driver;
-	  
-	  // home functional elements
+	// declare the variable
+
+	WebDriver driver;
+
+	// Constructor
+
+	public Home(WebDriver driver) {
+		this.driver = driver;
+
+		// home functional elements
 	}
-  public WebElement enrollNow() {
-	  return driver.findElement(By.id(""));
-  }
 
+	public WebElement enrollNow() {
+		return driver.findElement(By.xpath(
+				"//*[@id=\"content\"]/div/section[1]/div[2]/div/div/section/div[3]/div[1]/div/div[2]/div/div/a/span/span"));
+	}
+
+	public WebElement browseCourse() {
+		return driver
+				.findElement(By.xpath("//*[@id=\"content\"]/div/section[2]/div/div[2]/div/div/div/div/a/span/span"));
+	}
+
+	public WebElement displayCourseName() {
+		return driver.findElement(By.xpath(
+				"//*[@id=\"content\"]/div/section[3]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/h3/a"));
+
+	}
+
+	public WebElement displayCourseDuration() {
+		return driver.findElement(By.xpath(
+				"//*[@id=\"content\"]/div/section[3]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div[2]/span[2]"));
+	}
+
+	public WebElement numberOfCourseSubscribers() {
+		return driver.findElement(By.xpath(
+				"//*[@id=\"content\"]/div/section[3]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[2]/div[1]/span[2]"));
+	}
+
+	public WebElement courseOwner() {
+		return driver.findElement(By.xpath(
+				"//*[@id=\"content\"]/div/section[3]/div/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[1]/div[3]/div/a"));
+	}
 }
